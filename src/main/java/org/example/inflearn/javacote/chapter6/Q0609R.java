7952a6945669d09e82ad65a0881ed3f5e208a294
dvlpsjh <dvlpsjh@gmail.com>
2023-03-13 16:48:08 +0900

    simple changes before pull

--- src/main/java/org/example/inflearn/javacote/chapter6/Q0609R.java
+++ src/main/java/org/example/inflearn/javacote/chapter6/Q0609R.java
@@ -5,15 +5,16 @@ import jdk.jfr.Name;
 import java.util.Arrays;
 import java.util.Scanner;
 
-@Name("뮤직비디오(결정알고리즘)") // 결정 알고리즘 : 이분검색을 이용
-public class Q0609 {
+@Name("결정 알고리즘 - 복습")
+public class Q0609R {
     public static void main(String[] args) {
-        Q0609 t = new Q0609();
+        Q0609R t = new Q0609R();
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int[] arr = new int[n];
 
+        int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
@@ -23,14 +24,13 @@ public class Q0609 {
 
     private int solution(int n, int m, int[] arr) {
         int answer = 0;
-        // reduction ?
         int lt = Arrays.stream(arr).max().getAsInt();
         int rt = Arrays.stream(arr).sum();
 
         while(lt <= rt) {
-            int mid = (lt+rt) / 2;
+            int mid = (lt + rt) / 2;
 
-            if (count(arr, mid) <= m) {
+            if(count(arr, mid) <= m) {
                 answer = mid;
                 rt = mid - 1;
             } else {
@@ -41,16 +41,16 @@ public class Q0609 {
         return answer;
     }
 
-    private int count(int[] arr, int capacity) {
+    public int count(int[] arr, int capacity) {
         int cnt = 1;    // DVD장수
         int sum = 0;    // DVD에 담아내는 곡들의 합
 
         for (int x : arr) {
-            if(sum + x > capacity) {
+            if(sum + x <= capacity) {
+                sum += x;
+            } else {
                 cnt++;
                 sum = x;
-            } else {
-                sum += x;
             }
         }
 
